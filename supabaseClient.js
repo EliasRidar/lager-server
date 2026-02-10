15b1963ffdac064c592fae8d20c5872ba49f8407
EliasRidar <118311081+EliasRidar@users.noreply.github.com>
2026-02-10 01:13:45 +0100

    supabaseClient.js

---
+++ supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://noxrffgpooxhgbgxvnyw.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5veHJmZmdwb294aGdiZ3h2bnl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA2NzY3NzEsImV4cCI6MjA4NjI1Mjc3MX0.FynZeHNEl_fbZL2esohFpdwZzKbMy66rEHDypOGULM0'
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
